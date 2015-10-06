7652cc3718d3aeb66df32eb0da596936f34bdbd6
Thierry Lacour <thi@praqma.net>
2015-10-06 13:07:02 +0200

    Added option to load command from env var

--- src/main/groovy/ClearCaseToGit.groovy
+++ src/main/groovy/ClearCaseToGit.groovy
@@ -5,4 +5,8 @@ config.scriptBaseClass = 'ScriptBase'
 
 def shell = new GroovyShell(config)
 
-shell.evaluate(new File("command.groovy"))
+def envCmd = System.getenv()["CCTOGIT_COMMAND"]
+if (envCmd)
+    shell.evaluate(envCmd)
+else
+    shell.evaluate(new File("command.groovy"))
