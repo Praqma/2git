1eee5bb521a16cc0a6dfc19fcceaa3e360ca882d
Thierry Lacour <thi@praqma.net>
2015-12-04 12:25:13 +0100

    Added cmd action.

--- src/main/groovy/migration/ActionsContext.groovy
+++ src/main/groovy/migration/ActionsContext.groovy
@@ -2,16 +2,32 @@ package migration
 
 import git.Git
 import groovy.text.SimpleTemplateEngine
+import net.praqma.util.execute.CommandLine
 
 class ActionsContext {
     List<Action> actions = []
 
-    def void git(String cmd) {
+    def void git(String command) {
         actions.add(new Action() {
             @Override
             void act(HashMap<String, Object> extractionMap) {
-                def expandedCommand = new SimpleTemplateEngine().createTemplate(cmd).make(extractionMap).toString()
+                def expandedCommand = new SimpleTemplateEngine().createTemplate(command).make(extractionMap).toString()
                 Git.callOrDie(expandedCommand)
+            }
+        })
+    }
+
+    def void cmd(String command) {
+        def scriptPath = new File(getClass().protectionDomain.codeSource.location.path).parent
+        cmd(command, scriptPath)
+    }
+
+    def void cmd(String command, String path) {
+        actions.add(new Action() {
+            @Override
+            void act(HashMap<String, Object> extractionMap) {
+                def expandedCommand = new SimpleTemplateEngine().createTemplate(command).make(extractionMap).toString()
+                CommandLine.newInstance().run(expandedCommand, new File(path)).stdoutBuffer.eachLine { line -> println line}
             }
         })
     }
