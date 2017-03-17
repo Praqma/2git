efd77bb84f7138f064ec53e19f7355c893d07623
cssr <claus.schneider-ext@man-eu.com>
2017-03-17 14:57:54 +0100

    Configurable baseline_history.sh for query and cached projects

--- baseline_history.sh
+++ baseline_history.sh
@@ -8,6 +8,10 @@
 BASELINE_PROJECT=$1
 UNTIL_PROJECT=$2
 
+if [ "${query_type}X" == "X" ] ; then
+   export query_type="all_static"
+fi
+
 #UNTIL_SUBPROJECT=$3
 #BASELINE_SUBPROJECT=$4 #"ems_bus~1_20131002"
 
@@ -23,7 +27,7 @@ until [ "${BASELINE_PROJECT}X" == "X" -o "$BASELINE_PROJECT" == "$UNTIL_PROJECT"
 #	ccm rp -sh folders $BASELINE_PROJECT -u -f "%displayname" >> $OUTPUTFILE
     BASELINE_PROJECT=`printf "${BASELINE_PROJECT}" | sed -e 's/xxx/ /g' `
 	query="is_baseline_project_of('${BASELINE_PROJECT}:project:1')"
-	BASELINE_PROJECT=`ccm query "is_baseline_project_of('${BASELINE_PROJECT}:project:1')" -u -f "%name~%version" | sed -e 's/ /xxx/g'` 
+	BASELINE_PROJECT=`ccm query "is_baseline_project_of('${BASELINE_PROJECT}:project:1')" -u -f "%name~%version" | sed -e 's/ /xxx/g'`
 	bl_print=`printf "${BASELINE_PROJECT}" | awk -F"~" '{print $2}'`
 	if [ "${BASELINE_PROJECT}X" != "X" ] ; then
 		printf "${BASELINE_PROJECT} -> "
@@ -38,12 +42,17 @@ handle_baseline2(){
   local CURRENT_PROJECT=$1
 	local inherited_string_local=$2
 	proj_name=`printf "${CURRENT_PROJECT}" | sed -e 's/xxx/ /g' | awk -F"~" '{print $1}'`
-  proj_version=`printf "${CURRENT_PROJECT}" | sed -e 's/xxx/ /g' | awk -F"~" '{print $2}'`
-# full static version history	query="has_baseline_project('${proj_name}~${proj_version}:project:1') and ( status='integrate' or status='test' or status='sqa' or status='released' )"
-# static projects but leave out non-released that is not in use as a baseline  
+    proj_version=`printf "${CURRENT_PROJECT}" | sed -e 's/xxx/ /g' | awk -F"~" '{print $2}'`
+if [ "${query_type}X" == "all_staticX" ] ; then
+    # All status versions
+    query="has_baseline_project('${proj_name}~${proj_version}:project:1') and ( status='integrate' or status='test' or status='sqa' or status='released' )"
+fi
+if [ "${query_type}X" == "released_in_baselinesX" ] ; then
+    # static projects but leave out non-released that is not in use as a baseline
 	query="has_baseline_project('${proj_name}~${proj_version}:project:1') and ( status='released' ) or \
         (is_baseline_project_of(has_baseline_project(has_baseline_project('${proj_name}~${proj_version}:project:1') and ( status='integrate' or status='test' or status='sqa' or status='released' ))))"
-	local SUCCESSOR_PROJECTS=`ccm query "${query}" -u -f "%name~%version" | sed -e 's/ /xxx/g'` 
+fi
+	local SUCCESSOR_PROJECTS=`ccm query "${query}" -u -f "%name~%version" | sed -e 's/ /xxx/g'`
   for SUCCESSOR_PROJECT in ${SUCCESSOR_PROJECTS} ; do
 		local inherited_string="${inherited_string_local} -> \"${BASELINE_PROJECT}\""
     printf "$SUCCESSOR_PROJECT@@@$CURRENT_PROJECT\n" >> ${projects_file}
@@ -57,9 +66,11 @@ bl_version=`printf "${BASELINE_PROJECT}" | awk -F"~" '{print $2}'`
 
 export projects_file="./projects.txt"
 #rm -f ${projects_file}
-if [ -e ${projects_file} ] ; then
-  cat ${projects_file}
-  exit 0
+if [ "${use_cached_project_list}X" == "trueX" ]; then
+  if [ -e ${projects_file} ] ; then
+    cat ${projects_file}
+    exit 0
+  fi
 fi
 
 echo "$BASELINE_PROJECT@@@${init_project_name}~init" > ${projects_file}
