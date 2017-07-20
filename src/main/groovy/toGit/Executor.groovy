9bcc2cbe3bf802f4e7b62d74d9d3e335be1628c0
Thierry Lacour <thi@praqma.net>
2017-07-20 15:11:52 +0200

    Fix 127. Add script location to classpath.

--- src/main/groovy/toGit/Executor.groovy
+++ src/main/groovy/toGit/Executor.groovy
@@ -9,30 +9,31 @@ public class Executor {
     final static log = LoggerFactory.getLogger(this.class)
 
     GroovyShell execute(String[] args) {
-        File commandFile = null
         if (!args) {
-            log.error("Missing command file parameter")
+            log.error("Missing migration script parameter")
             System.exit(1)
-        } else {
-            commandFile = new File(args[0])
-            if (!commandFile.exists() || commandFile.isDirectory()) {
-                log.error("Cannot find command file '$commandFile'")
-                System.exit(1)
-            }
         }
 
-        def scriptDir = new File(getClass().protectionDomain.codeSource.location.path).parent
-        def config = new CompilerConfiguration()
-        config.setClasspath(scriptDir)
-        config.scriptBaseClass = 'toGit.ScriptBase'
+        File migrationScript = new File(args[0])
+
+        if (!migrationScript.exists() || migrationScript.isDirectory()) {
+            log.error("Cannot find migration script '$migrationScript'")
+            System.exit(1)
+        }
+
+        def config = new CompilerConfiguration(
+                scriptBaseClass: 'toGit.ScriptBase',
+                classpath: migrationScript.absoluteFile.parent
+        )
+
         def binding = new Binding(args as String[])
         if (args.length > 1) {
             parseParameters(binding, args[1..-1] as String[])
         }
 
         MigrationManager.instance.reset()
-        def shell = new GroovyShell(binding, config)
-        shell.evaluate(commandFile)
+        def shell = new GroovyShell(this.class.classLoader, binding, config)
+        shell.evaluate(migrationScript)
         return shell
     }
 
